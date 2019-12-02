276a6a6099571472ef2faa6d68b49ecda63f5fef
NianDUI <37703723+NianDUI@users.noreply.github.com>
2019-12-02 22:16:46 +0800

    springboot 03 logging o1

---
+++ 06springboot/spring-boot-03-logging/src/test/java/top/niandui/springboot/SpringBoot03LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package top.niandui.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
