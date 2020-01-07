d45bb57b4db1f8422db7b72501dddb99276715df
NianDUI <37703723+NianDUI@users.noreply.github.com>
2020-01-07 22:06:09 +0800

    springboot 06 六、数据访问 2、切换数据源 druid 01

--- 06springboot/spring-boot-06-data01-jdbc/src/main/java/top/niandui/springboot/SpringBoot06DataJdbcApplication.java
+++ 06springboot/spring-boot-06-data01-jdbc/src/main/java/top/niandui/springboot/SpringBoot06DataJdbcApplication.java
@@ -13,6 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  *      JdbcTemplateAutoConfiguration
  *          为我们自动注入了：JdbcTemplate 和 NamedParameterJdbcTemplate
+ *
+ * 2、切换数据源
+ * 引入数据源jar包，
+ * 引入Druid包需要配置，druid-spring-boot-starter包不需要type配置
+ *      配置数据源spring.datasource.type=com.alibaba.druid.pool.DruidDataSource
+ * 进行Druid数据源配置DruidConfig.java
+ *
  */
 @SpringBootApplication
 public class SpringBoot06DataJdbcApplication {
